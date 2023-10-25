45fd15186a78e41b9af4424b34e36cd40b6ce12a
wannabballer74 <pat.and.randi@gmail.com>
2023-10-24 19:55:48 -0700

    Added main method to print hello world

--- src/CIS233J_Lab4/HelloWorld.java
+++ src/CIS233J_Lab4/HelloWorld.java
@@ -1,4 +1,11 @@
 package CIS233J_Lab4;
 
-public class HelloWorld {
+public class HelloWorld
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
 }
