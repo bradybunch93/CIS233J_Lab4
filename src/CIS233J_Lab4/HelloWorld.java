e414d9b37a4761d39a1910e97782354d5a25f605
bradybunch93 <88181649+bradybunch93@users.noreply.github.com>
2023-10-24 23:24:49 -0700

    Update HelloWorld.java

--- src/CIS233J_Lab4/HelloWorld.java
+++ src/CIS233J_Lab4/HelloWorld.java
@@ -1,6 +1,14 @@
 package CIS233J_Lab4;
-
 import java.util.*;
+
+/**
+ * Simple HelloWorld class for Part 2 of Lab 4 "GitHub Project".
+ * Just to demonstrate ability to use GitHub with Git.
+ *
+ * @author Pat Brady
+ * @version 2023.10.24
+ */
+
 
 public class HelloWorld
 {
